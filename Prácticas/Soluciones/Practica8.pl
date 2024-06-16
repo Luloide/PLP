4e75db76aed8ece327729657d679a7f5eda8ef1c
Luloide <89493595+Luloide@users.noreply.github.com>
2024-06-16 13:27:45 -0300

    avances

--- Prácticas/Soluciones/Practica8.pl
+++ Prácticas/Soluciones/Practica8.pl
@@ -1,6 +1,6 @@
 %ejercicio1
 
-%base_de_conocimento_que_me_dan
+%base de conocimento que me dan
 padre(juan, carlos).
 padre(juan, luis).
 padre(carlos, daniel).
@@ -10,4 +10,7 @@ padre(luis, manuel).
 padre(luis, ramiro).
 abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
 
-%1
+% 1) juan
+hijo(X,Y) :- padre(Y,X).
+hermano(X,Y) :- padre(Z,X), padre(Z,Y), X \= Y.
+decendiente(X,Y) :- padre(Y,Z), padre(Z,X).

58bd18daa185708c9417f7ed00a8688afe43b734
Luloide <89493595+Luloide@users.noreply.github.com>
2024-06-16 09:14:53 -0300

    steando unas cosas

---
+++ Prácticas/Soluciones/Practica8.pl
@@ -0,0 +1,13 @@
+%ejercicio1
+
+%base_de_conocimento_que_me_dan
+padre(juan, carlos).
+padre(juan, luis).
+padre(carlos, daniel).
+padre(carlos, diego).
+padre(luis, pablo).
+padre(luis, manuel).
+padre(luis, ramiro).
+abuelo(X,Y) :- padre(X,Z), padre(Z,Y).
+
+%1
